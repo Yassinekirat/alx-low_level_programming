3e4e7d8b6df3e92648d90f5486718a9b0a6aae11
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-07 07:08:03 -0700

    adding betty documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
+/*
+* Use the function puts
+* You are not allowed to use printf
+* Your program should end with the value 0
+*/
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
