bd4042c85fb9a5a189be03e1e4ca4d5b24215535
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-07 08:37:10 -0700

    task 4 after 4000 years

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Description: Printing in C using puts function
- *
- * return: Always 0 (Success)
-*/
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return	(0);
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
 }
