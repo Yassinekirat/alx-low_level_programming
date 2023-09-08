b38457a0e191364c98005de5025f4083c9a6d105
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-08 06:27:40 -0700

    task 12 very challenging

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (a <= 99)
+	{
+		b = a;
+		while (b <= 99)
+		{
+			if (a != b)
+			{
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(' ');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
+
+				if (!(a == 98 || b == 99))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			b++;
+		}
+		a++;
+	}
+putchar('\n');
+return (0);
+}
