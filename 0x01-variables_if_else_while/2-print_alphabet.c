bcd938b6d8d5cb13486e6eb76285f1bcd760b6a4
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-08 03:02:05 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 'a';
+
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+	return (0);
+}
