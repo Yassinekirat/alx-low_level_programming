5117f085af173f2f8517ffd46351e3388f33dd3d
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-12 12:21:28 -0700

    task 9 maybe its been 5000 years

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ */
+
+void times_table(void)
+{
+	int n1;
+	int n2;
+	int result;
+
+	for (n1 = 0; n1 < 10 ; n1++)
+	{
+		putchar('0');
+		for (n2 = 0; n2 < 10; n2++)
+		{
+			_putchar(',');
+			result = n1 * n2;
+			if (result < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+		else
+		{
+			_putchar(' ');
+			_putchar((result / 10) + '0');
+		}
+		_putchar(' ');
+		_putchar((result % 10) + '0');
+		}
+	_putchar ('\n');
+	}
+}
