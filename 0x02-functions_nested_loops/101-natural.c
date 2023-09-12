17cc7dc258eb3aba068d83acf207fad176fa7608
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-12 14:58:04 -0700

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int result;
+	int n;
+
+	n = 0;
+	while (n < 1024)
+	{
+		if (i % 5 == 0 || i % 3 == 0)
+			result = result + n;
+		n++
+	}
+	printf("%d\n", result);
+
+	return (0);
+}
