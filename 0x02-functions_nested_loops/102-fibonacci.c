ab2597a2cfd96dbeec6b81e877b5dfc8534729c0
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-12 16:32:40 -0700

    task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int c;
+	unsigned long f1, f2, tot;
+
+	f1 = 0;
+	f2 = 1;
+
+	for (c = 0; c <= 49; c++)
+	{
+		tot = f1 + f2;
+		printf("%lu", tot);
+
+		f1 = f2;
+		f2 = sum;
+
+		if (c == 49)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
