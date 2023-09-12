6e515d1e35c8480d8a3b82ee3a887d1526e31a2d
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-12 12:26:24 -0700

    task 9 for sure this time

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,14 +20,11 @@ void times_table(void)
 			if (result < 10)
 			{
 				_putchar(' ');
-				_putchar(' ');
 			}
 		else
 		{
-			_putchar(' ');
 			_putchar((result / 10) + '0');
 		}
-		_putchar(' ');
 		_putchar((result % 10) + '0');
 		}
 	_putchar ('\n');
