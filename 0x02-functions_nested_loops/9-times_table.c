24ed5f7702a41bc9e32b0e831d42b04480df5da7
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-12 13:09:01 -0700

    can i go to sleep?

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,19 +15,20 @@ void times_table(void)
 		_putchar('0');
 		for (n2 = 0; n2 < 10; n2++)
 		{
-			_putchar(',');
-			_putchar(' ');
-
 			result = n1 * n2;
+
 			if (result < 10)
 			{
+				_putchar(' ');
 				_putchar(' ');
 			}
 			else
 			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
 			}
-		_putchar((result % 10) + '0');
 		}
 	_putchar ('\n');
 	}
