28e551b6fbd2f10e65bff82bbe826e703be7b5f3
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-14 04:55:57 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line -  draws a straight line
+ *
+ * @n: n -  integer to be tested
+ *
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+	int l;
+
+	if (l > 0)
+	{
+		for (l = 1; l <= n; l++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
