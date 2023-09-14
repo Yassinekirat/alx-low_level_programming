ee711092799d5ca9329d1ce959fcaa9b57f333eb
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-14 05:55:38 -0700

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square -  prints a square
+ *
+ * @size: integer to be tested
+ *
+ * Return:
+ */
+void print_square(int size)
+{
+	int r, c;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (r = 1 ; r <= size; r++)
+		{
+			for (c = 1; c <= size; c++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n')
+		}
+}
