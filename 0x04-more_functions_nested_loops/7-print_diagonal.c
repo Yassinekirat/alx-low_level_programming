bc7d26a8ca02a1a3431cbd282cb0f0a1596cc11b
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-14 05:15:01 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line
+ *
+ * @n: n - integer to be tested
+ *
+ * Return: Always 0 (Success)
+ */
+void print_diagonal(int n)
+{
+	int row, vide;
+
+	if (n > 0)
+	{
+		for (row = 1; row <= n; row++)
+		{
+			for (vide = 1; vide <= row ; vide++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
