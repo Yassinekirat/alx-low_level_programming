a72ec1fa9e1cf10a134085b8df0fa33656672b76
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-14 04:09:19 -0700

    task 4 typo

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * print_most_numbers - prints the numbers, from 0 to 9 exept from 2 & 4
  *
@@ -8,9 +8,9 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9 ; i++)
+	for (i = 0; i < 10 ; i++)
 	{
-		if (!(i == 2 && i == 4))
+		if (i != 2 && i != 4)
 		{
 		_putchar(i + '0');
 		}
