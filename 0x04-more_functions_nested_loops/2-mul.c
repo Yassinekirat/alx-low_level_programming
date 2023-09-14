076ee6fa3d24d1d9d97edf8c97e208eece6f75b4
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-14 03:27:24 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * mul -  mltiplies two intergers.
+ *
+ * @a: a - number to be tested
+ *
+ * @b: b - number to be tested
+ *
+ * Return: Always the multiplication result
+ */
+int mul(int a, int b)
+{
+	int x;
+
+	x = a * b;
+
+	return (x);
+}
