933db1465111ff8c6d96c9cbc87469a43f933af2
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-14 06:20:55 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz-Buzz
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 1; number <= 100; number++)
+	{
+		if (number % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (number % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (number % 3 == 0 && number % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", number);
+		}
+		if (number != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
