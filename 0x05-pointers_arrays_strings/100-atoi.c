927d1e0e3a107af41840917aa11d1c04666c9062
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-18 10:01:46 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _atoi - function that converts a string to an integer
+ *
+ * @s: string to be tested
+ *
+ * Return: n * sign
+ */
+int _atoi(char *s)
+{
+	int sign, l;
+	unsigned int n;
+
+	n = 0;
+	sign = 1;
+	
+	for (; *s; *s++)
+	{
+		if (*s == 45)
+		{
+			sign = sign * -1;
+		}
+		else if(*s >= 48 && *s <= 57)
+		{
+			num = (num * 10) + (*s - 48);
+		else if (num > 0)
+		{
+			break;
+		}
+	}
+}	return (n * sign);
