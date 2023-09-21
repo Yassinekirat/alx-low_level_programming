8dac0a7598a850d1d257648f3018b19d5f9df587
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-21 10:31:53 -0700

    task 6 tryin fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,15 +13,17 @@ char *cap_string(char *cap)
 {
 	int c;
 
-	for (c = 0; cap[c]; c++)
+	for (c = 0; cap[c] != '\0'; c++)
 	{
-		for (; !(cap[c] >= 'a' && cap[c] <= 'z'); c++);
-			;
+		/*checking all conditions*/
 		if (cap[c] == ',' || cap[c] == ';' || cap[c] == '.' || cap[c] == '!' ||
 		    cap[c] == '?' || cap[c] == '"' || cap[c] == '(' || cap[c] == ')' ||
 		    cap[c] == '{' || cap[c] == '}' || cap[c] == ' ' || cap[c] == '\n' ||
-		    cap[c] == '\t')
-			cap[c] = cap[c] - 32;
+		    cap[c] == '\t' || cap[c] == 0)
+		{
+			if (cap[c + 1] >= 'a' && cap[c + 1] <= 'z')
+				cap[c] = cap[c] - 32;
+		}
 	}
 	return (cap);
 }
