dc4dc1a96c5094bbc29d2007d8f9b7726eded899
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-21 08:06:31 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,5 +11,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
+	int l, cmp;
 
+	for (l = 0; s1[l] && s2[l] != '\0'; l++)
+	{
+		if (s1[l] != s2[l])
+		{
+			cmp = s1[l] - s2[l];
+			return (cmp);
+		}
+	}
+	return (0);
 }
