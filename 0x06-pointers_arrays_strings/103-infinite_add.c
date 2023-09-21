37ad32505e56c063d86a6034613aaf411874cc2a
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-21 12:45:57 -0700

    task 11

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * infinite_add - adds 2 numbers
+ * @n1: num 1
+ *
+ * @n2: num 2
+ *
+ * @r: buffer
+ *
+ * @size_r: buffer size
+ *
+ * Return: summ
+ *
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	return (summ);
+}
