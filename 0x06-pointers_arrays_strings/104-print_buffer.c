c3397d11339e93106af5f6b02830fb68618a29b6
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-21 12:52:37 -0700

    task 12

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_buffer - print the content of size bytes of the buffer
+ *
+ * @b: string to be tested
+ *
+ * @size: size
+ *
+ * Return: nothing
+ *
+ */
+void print_buffer(char *b, int size)
+{
+}
