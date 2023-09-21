b987f1cfa854ed8830191246830404e0e3b4b1c0
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-21 07:49:41 -0700

    task 3 skip for now

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares dest and src
+ *
+ * @s1: string to be tested
+ *
+ * @s2: string to be tested
+ *
+ * Return: comparison
+ */
+int _strcmp(char *s1, char *s2)
+{
+
+}
