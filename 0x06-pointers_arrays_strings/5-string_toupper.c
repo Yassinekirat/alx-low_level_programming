71a069d5fb03353b3fc884e8af82b854abd30ef1
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-21 08:48:36 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - reverse array
+ *
+ * @up: string to be tested
+ *
+ *
+ * Return: uppercase
+ */
+char *string_toupper(char *up)
+{
+	int l, u;
+
+	for (l = 0; up[l] != '\0'; l++)
+	{
+		if (up[l] >= 'a' && up[l] <= 'z')
+		{
+			u = ('z' - 'a');
+			up[l] = up[l] - u;
+		}
+	}
+	return (up);
+}
