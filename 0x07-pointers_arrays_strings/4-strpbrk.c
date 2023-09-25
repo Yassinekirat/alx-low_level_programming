a9d251e39dda1798c422b513373308039e67bd64
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-25 10:34:58 -0700

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strpbrk - a function that fills memory with a constant byte
+ *
+ * @s: string t be tested
+ *
+ * @accept: character to be tested
+ *
+ * Return: 0 success
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	return (0);
+}
