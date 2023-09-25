e91cd3f8655fcc16aa21b34ccff8f0b560515669
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-25 10:05:41 -0700

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,16 @@
+
+#include "main.h"
+
+/**
+ * _strspn - a function that fills memory with a constant byte
+ *
+ * @s: pointer to memory area
+ *
+ * @accept: bytes
+ *
+ * Return: s pointer to constant byte
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	return (s);
+}
