858e3b8acfe519c1ca213dde9172ada4bbc2aa0a
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-25 08:54:11 -0700

    fix task 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-//Prototypes
+/*Prototypes*/
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
