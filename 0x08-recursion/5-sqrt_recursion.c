8537b8078fc65f32f225ca319db60de83a395c4e
Yassinekirat <yassine.elkirat@gmail.com>
2023-09-28 02:09:12 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * squar_root - a function that printf  the natural square root of a number
+ *
+ * @n: integer to be tested
+ *
+ * @num2: integer to be tested
+ *
+ * Return: squar root
+ */
+int square_root(int n, int num2)
+{
+	if (num2 * num2 > n)
+	{
+		return (-1);
+	}
+	if (num2 * num2 == n)
+	{
+		return (num2);
+	}
+	return (square_root(n, num2 + 1));
+}
+
+
+
+
+/**
+ * _sqrt_recursion - a function that printf the natural square root of a number
+ *
+ * @n: integer to be tested
+ *
+ * Return: squar root
+ */
+int _sqrt_recursion(int n)
+{
+	return (square_root(n, 1));
+}
