03545c6e7be4a7253f2830e13804d6693289748c
Yassinekirat <yassine.elkirat@gmail.com>
2023-10-03 18:49:42 -0700

    file of task 6 for later

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strtow - that splits a string into words
+ * @str: string  to be tested
+ *
+ * Return:  pointer or Null if insufficient memory
+ */
+char **strtow(char *str)
+{
+	return (str);
+}
