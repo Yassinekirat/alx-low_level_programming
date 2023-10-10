d1a081686ad1473fa06686e494802ae6da870b51
Yassinekirat <yassine.elkirat@gmail.com>
2023-10-10 13:17:38 -0700

    task 0 dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for a dog identity
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the name of the owner
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif /*DOG_H*/
