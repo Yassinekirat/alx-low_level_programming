07b8c422adcc9eceb98b88bbdb2411f9dceeb034
Yassinekirat <yassine.elkirat@gmail.com>
2023-10-12 04:57:01 -0700

    task 3 looks good

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all -  function that prints anything
+ * @format: format to be tested
+*/
+
+void print_all(const char * const format, ...)
+{
+	char *sep = "", *str;
+	int i;
+
+	va_list args;
+
+	va_start(args, format);
+
+	if (format)
+	{
+		for (i = 0; format[i]; i++)
+		{
+			if (format[i] == 'c')
+			{
+				printf("%s%c", sep, va_arg(args, int));
+			}
+			else if (format[i] == 'i')
+			{
+				printf("%s%d", sep, va_arg(args, int));
+			}
+			else if (format[i] == 'f')
+			{
+				printf("%s%f", sep, va_arg(args, double));
+			}
+			else if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s%s", sep, str);
+			}
+			else
+			{
+				continue;
+			}
+			sep = ", ";
+		}
+	}
+
+	putchar('\n');
+	va_end(args);
+}
