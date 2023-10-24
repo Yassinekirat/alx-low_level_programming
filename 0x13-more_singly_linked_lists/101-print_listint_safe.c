bd4cbab8f7f40ff68b1c86ec74064f3f05e0c2d3
Yassinekirat <yassine.elkirat@gmail.com>
2023-10-23 21:22:34 -0700

    pushing file for later

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - Write a function that prints a linked list
+ *
+ * @head: Pointer to be tested
+ *
+ * Return: Number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	return(0)
+}
