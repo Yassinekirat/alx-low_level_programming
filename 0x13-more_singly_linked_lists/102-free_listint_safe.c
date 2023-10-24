e954b1972d1615affc5aae76e7d8fa7ae6be06b1
Yassinekirat <yassine.elkirat@gmail.com>
2023-10-23 21:55:38 -0700

    adding file for later too

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - a function that frees a list
+ *
+ * @h: double pointer to be tested
+ *
+ * Return: the size of the list that was free’d
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	return (-1);
+}
