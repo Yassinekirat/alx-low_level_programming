8e7e459d4d9dd1010fe611c6abf83544c7fad4cf
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2023-10-26 12:40:58 +0100

    task 3 11

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* set_bit - converts the bit in @n in position @index to 1
+* @index: index to be tested
+* @n: integer to be tested
+*
+* Return: 1 if it worked, or -1 if an error occurred
+*/
+
+int set_bit(unsigned long int *n, unsigned long int index)
+{
+	if ((index >= sizeof(long) * 8))
+	{
+		return (-1);
+	}
+	*n |= 1UL << index;
+	return (1);
+}
